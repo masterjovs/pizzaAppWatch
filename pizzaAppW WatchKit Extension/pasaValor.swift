c2591fe2581c190c3dbc26aa2964d54ab5a65a52
JoseOrlandoVargasSanchez <JoseVargas@MacBook-Pro-de-JoseOrlandoVargasSanchez.local>
2016-05-15 23:50:14 -0500

    first commit

---
+++ pizzaAppW WatchKit Extension/pasaValor.swift
@@ -0,0 +1,13 @@
+//
+//  pasaValor.swift
+//  pizzaAppW
+//
+//  Created by JoseOrlandoVargasSanchez on 15/05/16.
+//  Copyright © 2016 JoseOrlandoVargasSanchez. All rights reserved.
+//
+
+import WatchKit
+
+class pasaValor: NSObject {
+
+}
